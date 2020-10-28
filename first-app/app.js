1f207ef9e4a3f43d2bd0de283a426a9a41c1a337
levi-sr93 <levi-sr@outlook.com>
2020-10-28 08:07:43 -0300

    Global Object | Modules

--- first-app/app.js
+++ first-app/app.js
@@ -3,3 +3,22 @@ function sayHello(name) {
 }
 sayHello('Levi')
 
+// console.log(); //global module
+// setTimeout();
+// clearTimeout();
+// setInterval();
+// clearInterval();
+
+// //window object
+// global.setInterval();
+
+// var message = "this is a message"; // only accessible in this file. because of module system
+// console.log(global.message)
+
+// const sayHello = function () {
+
+// }
+
+// window.sayHello();
+
+console.log(module)
